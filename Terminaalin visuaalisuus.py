8b2bb60caf4d5251d07a88b8be8dd4bd4f5a59fc
Villeema <156772305+Villeema@users.noreply.github.com>
2024-03-04 11:01:15 +0200

    Terminaalin visuaalisuus.py

---
+++ Terminaalin visuaalisuus.py
@@ -0,0 +1,38 @@
+################## STEP ONE ####################
+# ALKUNÄYTTÖ MISSÄ SELITETÄÄN PELAAJALLE HOMMAN NIMI
+
+# 'def step_one():
+#     time.sleep(2)
+#
+#     print("""
+#                                         |
+#                                         |
+#                                         |
+#                                       .-'-.
+#                                      ' ___ '
+#                            ---------'  .-.  '---------
+#            _________________________'  '-'  '_________________________
+#             ''''''-|---|--/    \==][^',_m_,'^][==/    \--|---|-''''''
+#                           \    /  ||/   H   \||  \    /
+#                            '--'   OO   O|O   OO   '--'
+#
+#                                   S K Y H A W K
+#                                        v.1
+#     """)
+#
+#     time.sleep(4)
+#     os.system('cls')
+#
+#     print("""
+#                                Tervetuloa skyhawk peliin!
+#
+#     Tehtävänäsi on paikantaa varastettu lentokone/helikopteri satunnaiselta
+#     sijainnista euroopassa. Sijainti on joko lentoasemalla tai helikopterialustalla.
+#     Avaa valmiiksi selaimesta haluamasi karttaselain (suosittelemme google mapsia).
+#     """)
+#
+#     time.sleep(12)
+#     os.system("cls")'
+
+from art import *
+tprint("Hello World"), font = "standard"
