6546030b981c878dc78ce1a0d48d39eef1e33e6c
makyo <jonathan.shi@metropolia.fi>
2024-03-06 17:11:00 +0200

    lentoasema_random ja lentoasema_koko jäljellä

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def connection():
     )
 # funktio joka valitse random maan joka eurossa ja palauttaa sen returnilla
 def maa_random():
-    maa = "SELECT name FROM country WHERE continent = 'EU' ORDER BY name;"
+    maa = "SELECT name, iso_country FROM country WHERE continent = 'EU' ORDER BY RAND() LIMIT 1;"
     return
 
 # funktio joka valitsee random kaupungin maasta ja palauttaa sen returnilla
@@ -28,14 +28,15 @@ def kaupunki_random():
 # funktio joka valitsee random lentoaseman kaupungista, huom! jos lentoasema on yli 5
 def lentoasema_random():
 
-# funktio joka hakee lentoaseman koordinaatit, muuttaa niitä vähintään 100m erisuuntiin ja palauttaa ne returnilla (gps muodossa)
+# funktio joka hakee lentoaseman koordinaatit, tehdään game_logic.py[muuttaa niitä vähintään 100m erisuuntiin ja palauttaa ne returnilla (gps muodossa)]
 def lentoasema_koordinaatit():
     lentoasema_koordinaatti = "SELECT latitude_deg, longitude_deg FROM airport LIMIT 1;"
     return
 
 # funktio joka palauttaa kohteen tyypin (lentoasema tai heliport)
 def lentoasema_tyyppi():
-    lento_tai_heli = "SELECT type FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE airport.type != 'closed';"
+    lento_tai_heli = "SELECT type FROM airport JOIN country ON airport.iso_country = country.iso_country WHERE airport.type != 'closed' LIMIT 1;"
+    return
 
 # funktio joka palauttaa lentoaseman koon, jos kyseessä on heliport, eli kokoa ei ole niin palautetaan False.
 def lentoasema_koko():
