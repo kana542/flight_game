8b0c05eb2db231b12c8b4f563546e4ac148e33e6
joel22k <joelketo@metropolia.fi>
2024-03-06 22:00:07 +0200

    lentoasema random ja lentoasema koko

--- database.py
+++ database.py
@@ -10,6 +10,7 @@ def kaupunki_random():
 # funktio joka valitseee random lentoaseman kaupungista, huom! jos lentoasema on yli 5
 def lentoasema_random():
     lentoasema_random_sql = "SELECT name FROM airport ORDER BY RAND() LIMIT 1;"
+
 # funktio joka hakee lentoaseman koordinaatit, muuttaa niitä vähintään 100m erisuuntiin ja palauttaa ne returnilla (gps muodossa)
 def lentoasema_koordinaatit():
 
@@ -18,3 +19,9 @@ def lentoasema_tyyppi():
 
 # funktio joka palauttaa lentoaseman koon, jos kyseessä on heliport, eli kokoa ei ole niin palautetaan False.
 def lentoasema_koko():
+    lentoasema_koko_sql = "SELECT type FROM airport;"
+
+    if tulos == "heliport";
+        return False
+
+
